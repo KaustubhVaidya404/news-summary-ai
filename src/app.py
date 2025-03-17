74bbda4628cd2a26f58ea829909c869f97a54cbc
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2025-03-17 17:28:03 +0530

    updates to handle the sentiment analysis

---
+++ src/app.py
@@ -0,0 +1,6 @@
+from utils import expose
+
+company_name = input("Enter conpany name: \n")
+data = expose(company_name)
+
+print(data)
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ headers = {
 
 
 def extract_articles(company_name):
+    """Function to extract the articles from times of india website"""
+
     url = f"https://timesofindia.indiatimes.com/topic/{company_name}/news"
 
     try:
@@ -17,9 +19,37 @@ def extract_articles(company_name):
 
         data = soup.find_all('div', class_='VXBf7')
         if data:
+            articles = []
             for div in data:
-                return div.prettify()
+
+                title = div.find('div', class_='fHv_i').get_text(strip=True) if div.find('div', class_='fHv_i') else "No title"
+                date = div.find('div', class_='ZxBIG').get_text(strip=True) if div.find('div', class_='ZxBIG') else "No date"
+                summary = div.find('p', class_='oxXSK').get_text(strip=True) if div.find('p', class_='oxXSK') else "No summary"
+
+                article = {
+                    'title': title,
+                    'date': date,
+                    'summary': summary
+                }
+                articles.append(article)
+            return articles
         else:
             return "No data found."
-    except:
-        return f"Failed to fetch page. Status code: {result.status_code}"
+    except(e):
+        return f"Failed to fetch page. Status code: {result.status_code} {e}"
+
+
+def sentiment_analysis():
+    """Function to perform sentiment analysis"""
+    pass
+
+
+def comparative_analysis():
+    """Function to perform comparative sentiment analysis"""
+    pass
+    
+
+def expose(company_name):
+    """Root function which will be exposed"""
+    res = extract_articles(company_name)
+    return res
