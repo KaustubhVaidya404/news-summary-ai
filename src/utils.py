f3cbd997a69f3488934c41fa53855cb273df7f95
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2025-03-17 15:32:03 +0530

    updated to function

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 }
 
-company_name = input("Enter company name: \n")
 
-url = f"https://timesofindia.indiatimes.com/topic/{company_name}/news"
+def extract_articles(company_name):
+    url = f"https://timesofindia.indiatimes.com/topic/{company_name}/news"
 
-try:
-    result = requests.get(url, headers=headers)
-    print(result.status_code)
+    try:
+        result = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(result.text, 'html.parser')
+        soup = BeautifulSoup(result.text, 'html.parser')
 
-    data = soup.find_all('div', class_='VXBf7')
-    if data:
-        for div in data:
-            print(div.prettify())
-    else:
-        print("No data found.")
-except:
-    print(f"Failed to fetch page. Status code: {result.status_code}")
+        data = soup.find_all('div', class_='VXBf7')
+        if data:
+            for div in data:
+                return div.prettify()
+        else:
+            return "No data found."
+    except:
+        return f"Failed to fetch page. Status code: {result.status_code}"
